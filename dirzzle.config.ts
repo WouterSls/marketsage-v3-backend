109777806c40826f1cc35900ac8734addf8826e3
woutersls <phaerocode@hotmail.com>
2025-02-26 22:22:40 +0100

    init commit

---
+++ dirzzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "sqlite",
+  schema: "./src/lib/db/schema.ts",
+  out: "./js-build/src/lib/db/drizzle",
+  dbCredentials: {
+    url: "file:./data/db.sqlite",
+  },
+});
---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+import path from "path";
+import http from "http";
+import app from "./app";
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+const PORT = process.env.PORT || 8080;
+
+async function startServer() {
+  try {
+    const server = http.createServer(app);
+
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+    process.on("SIGINT", () => {
+      console.log("Gracefully shutting down server...");
+      server.close(() => {
+        console.log("Server closed");
+        process.exit(0);
+      });
+    });
+  } catch (error) {
+    console.error(`Failed to start server: ${error}`);
+    process.exit(1);
+  }
+}
+
+startServer();
