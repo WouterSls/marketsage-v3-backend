24a4651db45166fff0cb4873964411cfc46b5a2c
woutersls <phaerocode@hotmail.com>
2025-03-17 23:35:07 +0100

    chore: update route and openapi.yaml

--- src/api/token-monitor/TokenMonitorRouter.ts
+++ src/api/token-monitor/TokenMonitorRouter.ts
@@ -3,7 +3,9 @@ import { TokenMonitorController } from "./TokenMonitorController";
 
 const router = Router();
 
+router.get("/token-monitor/tokens/:address", TokenMonitorController.getTokenByAddress);
 router.get("/token-monitor/tokens", TokenMonitorController.getAllTokens);
+router.get("/token-monitor/buyable-tokens", TokenMonitorController.getBuyableTokens);
 router.get("/token-monitor/trades", TokenMonitorController.getAllTrades);
 router.get("/token-monitor/positions", TokenMonitorController.getAllPositions);
 router.get("/token-monitor/active-positions", TokenMonitorController.getActivePositions);
